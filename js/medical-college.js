99be9b68b59b060329612be6186fd094f216930c
V-dhumal <dhumalvaishnavi9356@gmail.com>
2024-01-16 18:20:56 +0530

    commit

--- js/medical-college.js
+++ js/medical-college.js
@@ -1,32 +1,53 @@
-var currentImg = 0;
-  var imgs = document.querySelectorAll('.slider img');
-  let dots = document.querySelectorAll('.dot');
-  var interval = 3000;
+function changeImage(fileName){
+  let img=document.querySelector('#image1');
+  img.setAttribute("src",fileName);
+}
+function changeImage1(fileName){
+  let img=document.querySelector('#image2');
+  img.setAttribute("src",fileName);
+  
+}
+function changeImage2(fileName){
+  let img=document.querySelector('#image3');
+  img.setAttribute("src",fileName);
+}
+function changeImage3(fileName){
+  let img=document.querySelector('#image4');
+  img.setAttribute("src",fileName);
+}
+function changeImage4(fileName){
+  let img=document.querySelector('#image5');
+  img.setAttribute("src",fileName);
+}
+function changeImage5(fileName){
+  let img=document.querySelector('#image6');
+  img.setAttribute("src",fileName);
+}
+function changeImage6(fileName){
+  let img=document.querySelector('#image7');
+  img.setAttribute("src",fileName);
+}
+function changeImage7(fileName){
+  let img=document.querySelector('#image8');
+  img.setAttribute("src",fileName);
+}
+function changeImage8(fileName){
+  let img=document.querySelector('#image9');
+  img.setAttribute("src",fileName);
+}
 
-
-
-  var secondImageUrl = "../images/nagpur-1.png";
-  var thirdImageUrl = "../images/nagpur-3.png";
-
-  document.getElementById('img-2').src = secondImageUrl;
-  document.getElementById('img-3').src = thirdImageUrl;
-
-  var timer = setInterval(changeSlide, interval);
-  console.log(timer)
-  function changeSlide(n) {
-    for (var i = 0; i < imgs.length; i++) {
-      imgs[i].style.opacity = 0;
-      dots[i].className = dots[i].className.replace(' active', '');
-    }
-
-    currentImg = (currentImg + 1) % imgs.length;
-
-    if (n != undefined) {
-      clearInterval(timer);
-      timer = setInterval(changeSlide, interval);
-      currentImg = n;
-    }
-
-    imgs[currentImg].style.opacity = 1;
-    dots[currentImg].className +='active';
-  }
+let isOpen = false;
+function togglesidebae()
+{
+const sidebar = document.getElementById('sidebar');
+if(isOpen)
+{
+   sidebar.classList.add('sidebar-closed');
+   isOpen = false;
+}
+else
+{
+   sidebar.classList.remove('sidebar-closed');
+   isOpen = true;
+}
+}
